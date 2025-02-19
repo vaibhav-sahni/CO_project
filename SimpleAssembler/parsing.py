131bd67dc569d74718b5443e0aefc1cb20a0a2c6
Vaibhav Sahni <sahnidlf@gmail.com>
2025-02-19 18:45:53 +0530

    removed one bug

--- SimpleAssembler/parsing.py
+++ SimpleAssembler/parsing.py
@@ -25,4 +25,3 @@ def parse_J(inst,rd,imm):
     imm_parts = imm_bin[0] + imm_bin[10:20] + imm_bin[9] + imm_bin[1:9]
     return imm_parts + registers[rd] + opcode[inst]
 
-print(parse_B('beq','zero','zero',0))
